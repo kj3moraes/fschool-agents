e439872acfa646883c55911f4fe68e2a7f18e81e
Keane Moraes <lordvader3002@gmail.com>
2023-10-28 20:15:07 -0400

    made caller redundant

--- caller.py
+++ caller.py
@@ -1,20 +1,20 @@
-from web_searches import *
-from pathlib import Path
-import json
+# from web_searches import *
+# from pathlib import Path
+# import json
 
-textbook_path = Path("./textbooks/statistics.pdf")
-questions = ["what is the chi-squared distribution?"]
+# textbook_path = Path("./textbooks/statistics.pdf")
+# questions = ["what is the chi-squared distribution?"]
 
-unknowns = {
-    "what is the chi-squared distribution?": {
-        "chi-squared distribution",
-        "statistics",
-        "probability distributions"
-    } 
-}
+# unknowns = {
+#     "what is the chi-squared distribution?": {
+#         "chi-squared distribution",
+#         "statistics",
+#         "probability distributions"
+#     } 
+# }
 
 
-answers = get_answers(questions, unknowns)
+# answers = get_answers(questions, unknowns)
 
-with open("final_answers.json","w+") as f:
-    json.dump(answers, f, indent=4)
+# with open("final_answers.json","w+") as f:
+#     json.dump(answers, f, indent=4)
